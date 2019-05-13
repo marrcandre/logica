8192fee1cb8b948f70304b0497d47eba24f8e31b
Marco André Mendes <marcoandre@gmail.com>
2019-05-13 16:11:08 -0300

    dados: versão completa

--- 1info1-logica/dados.py
+++ 1info1-logica/dados.py
@@ -17,3 +17,14 @@ print("Dado 1:", dados1)
 print("Dado 2:", dados2)
 print("Dado 3:", dados3)
 print("Dado 4:", dados4)
+
+print("Jogador 1: ", jogador1)
+print("Jogador 2: ", jogador2)
+
+if jogador1 > jogador2:
+    print('Jogador 1 venceu!')
+else:
+    if jogador2 > jogador1:
+        print("Jogador 2 venceu!")
+    else:
+        print("Empate!")
