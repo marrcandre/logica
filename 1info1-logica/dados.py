32608c7f4b590be71a60852f1534399190798b36
Marco André Mendes <marcoandre@gmail.com>
2019-05-13 15:35:03 -0300

    dados: incluído cálculo da soma dos dados

--- 1info1-logica/dados.py
+++ 1info1-logica/dados.py
@@ -1,11 +1,17 @@
+# Programa dos dados
+# Marco André Mendes <marcoandre@gmail.com>
+
 from random import randint
 
-print("Jogo de Dados:")
+print("Jogo de Dados (v.0.1")
 
 dados1 = randint(1, 6)
 dados2 = randint(1, 6)
+jogador1 = dados1 + dados2
+
 dados3 = randint(1, 6)
 dados4 = randint(1, 6)
+jogador2 = dados3 + dados4
 
 print("Dado 1:", dados1)
 print("Dado 2:", dados2)
